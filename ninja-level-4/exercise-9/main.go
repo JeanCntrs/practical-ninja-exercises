ab92eb83ed62ff83c2bfa7f3fde71441be5a3f9a
Jean C. Contreras <jean.cntrs@gmail.com>
2020-11-01 11:07:10 -0300

    Practical exercise 9 of ninja lvl 4 was solved

---
+++ ninja-level-4/exercise-9/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := map[string][]string{
+		"eduar_tua":    {"computadoras", "montaña", "playa"},
+		"carlos_ramon": {"leer", "comprar", "música"},
+	}
+	x["juan_bimba"] = []string{"helado", "pintar", "bailar"}
+	x["luis_perez"] = []string{"trabajar", "playa", "cerveza"}
+	fmt.Println(x)
+
+	for key, value := range x {
+		fmt.Println("Registro:", key)
+		for k, v := range value {
+			fmt.Println("\t", k, v)
+		}
+	}
+}
