ac75561bec37d24497b6db7f9f34044573d97198
Jean C. Contreras <jean.cntrs@gmail.com>
2020-11-23 09:06:18 -0300

    Practical exercise 2 of ninja lvl 10 was solved

---
+++ ninja-level-10/exercise-2/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	sendOnly()
+	receiveOnly()
+}
+
+func sendOnly() {
+	// Send only
+	// cs := make(chan<- int)
+
+	// Bidirectional
+	cs := make(chan int)
+
+	go func() {
+		cs <- 42
+	}()
+	fmt.Println(<-cs)
+
+	fmt.Printf("------\n")
+	fmt.Printf("cs\t%T\n", cs)
+}
+
+func receiveOnly() {
+	// Receive only
+	// cr := make(<-chan int)
+
+	// Bidirectional
+	cr := make(chan int)
+
+	go func() {
+		cr <- 42
+	}()
+	fmt.Println(<-cr)
+
+	fmt.Printf("------\n")
+	fmt.Printf("cr\t%T\n", cr)
+}
---
+++ ninja-level-10/exercise-2/statement.md
@@ -0,0 +1,7 @@
+**Ejercicio Práctico #2**
+
+- Haz que este código funcione:
+    - https://play.golang.org/p/oB-p3KMiH6 
+        - Solución:  https://play.golang.org/p/isnJ8hMMKg 
+    - https://play.golang.org/p/_DBRueImEq 
+        - Solución: https://play.golang.org/p/mgw750EPp4 
